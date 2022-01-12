96b7f5f7a1a15af26e64cb4f1f86bb6a287ee2a8
Petri Silen <petri.silen@silensoft.com>
2022-01-12 20:20:26 +0200

    INTERNAL: Modified

--- scripts/helm-install.sh
+++ scripts/helm-install.sh
@@ -1,2 +1,7 @@
+helm status "$MICROSERVICE_NAME" >/dev/null 2>&1
 
-helm install -f helm/values/values-minikube.yaml $MICROSERVICE_NAME helm/$MICROSERVICE_NAME-1.0.0.tgz
+if [ $? -eq 0 ]; then
+  helm uninstall "$MICROSERVICE_NAME"
+fi
+
+helm install -f helm/values/values-minikube.yaml "$MICROSERVICE_NAME" helm/"$MICROSERVICE_NAME"-1.0.0.tgz
